d6e7f5beffca07615f42e013304c7a19177ad94c
Surojit Halder <jitsuro442@gmail.com>
2024-04-26 20:44:50 +0530

    26/4/24-----8:44

---
+++ home-work/assets/js/script.js
@@ -0,0 +1,141 @@
+
+
+function getcalculate( x,y,a){
+
+    var result = x + y ;
+    console.log("   Sum Result : ",result )
+};
+getcalculate(12,7);
+
+
+
+
+function getsub(a,b,c){
+
+    var result = a -  b
+    console.log(" Sub Result : ",result )
+}
+getsub(15,8)
+
+
+
+
+
+function getmultyply(a,b,){
+
+    var result = a * b;
+    console.log(" multiplyed Result : ",result );
+};
+getmultyply(15,8);
+
+
+
+
+
+
+
+function getdiv(a,b,){
+
+var result = a / b ;
+console.log(" Div Result : ",result );
+};
+getdiv(108,12);
+ 
+
+
+// const a = [1,2,3,4,5];
+// const slicedArray = a.slice(0, -2);
+// console.log(slicedArray)
+
+
+
+
+// const a = [1,2,3,4,5];
+// const slicedArray = a.slice(1, 4);
+// console.log(slicedArray)
+
+
+
+
+// const q = [1,2,3,4,5];
+// const splicedArray = q.splice(1, 4);
+// console.log(splicedArray)
+
+
+
+
+
+const a = [1,2,3,4,5];
+
+const slicedArray = a.slice(1, -2);
+
+console.log(slicedArray)
+
+
+
+
+const q = [1,2,3,4,5];
+const splicedArray = q.splice(-4);
+console.log(splicedArray)
+
+
+// const qualifyAge = 22
+
+// if (qualifyAge >= 28){
+
+//     alert('You Are Not A Audult.');
+// }else {
+//     alert( 'You Are Qualified.'  )
+// }
+
+
+
+// const m = 22;
+// var l = 12 ;
+
+
+// if (m + l == 34 + 1 ){
+
+//     alert(' right ');
+// }else {
+//     alert( ' wrong ' );
+// }
+        
+
+
+
+// var j = 23
+
+// if (j % 2 == 0){
+//     alert("Given Number Is Even")
+
+// }else{
+//     alert("Given Number Is Uneven")
+// }
+
+
+const Marvel_heros = ["Thor", "Spiderman", "Hulk"];
+const DC_heros = ["Superman", "Flash", "Batman"];
+
+ Marvel_heros.concat(DC_heros);
+console.log(DC_heros);
+alert(Marvel_heros)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
