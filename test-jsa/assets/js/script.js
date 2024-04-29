ca08a0b115a818454784a5c4631c383ed4901e54
Surojit Halder <jitsuro442@gmail.com>
2024-04-29 21:10:28 +0530

    last color currection

---
+++ test-jsa/assets/js/script.js
@@ -0,0 +1,82 @@
+
+function getsum(a,b){
+var sum = a+b;
+console.log("sum result : ", sum  );
+};
+
+getsum(20,30);
+
+
+
+function getSub(a,b){
+    var sub = a-b;
+    console.log("sub result:", sub)
+}
+getSub(19,9)
+
+
+
+function getMultification(x,y){
+    var multification = x*y;
+    console.log("Get multiply:", multification)
+};
+
+getMultification(10,19);
+
+
+getMultification(15,15);
+
+
+
+function getMod(c,z){
+    var mod = c % z;
+console.log("moduler divition result:", mod  )
+};
+getMod(11,3);
+
+getMod(33,3);
+
+getMod(99,7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
